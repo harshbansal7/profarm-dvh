74b44db0290b787333a64ba6ee079bcb806dae6a
Harsh Bansal <harshbansal.contact@gmail.com>
2022-03-19 21:14:55 +0530

    ML Part

--- dfhmain/main_app/views.py
+++ dfhmain/main_app/views.py
@@ -28,4 +28,71 @@ cropcats = (ndf['Crop'].cat.categories)
 
 # Create your views here.
 def home(request):
-        return render(request, 'index.html')
+    if request.GET :
+        dict = request.GET
+        if(dict['service_name'] == 'NPK Calculator'):
+            try:
+                received_data = [dict['nitrogen'], dict['phosphorus'], dict['potassium'], dict['temp'], dict['humidity'], dict['phval'], dict['rainfall']]
+                testing_data = [np.array(received_data, dtype='float64')]
+
+                df_crop = pd.read_csv('main_app/csvfiles/npk.csv')
+                X = df_crop.drop(columns = 'label')
+                y = df_crop['label']
+
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+
+                knn = KNeighborsClassifier(n_neighbors=3)
+                knn.fit(X_train, y_train)
+
+                final_prediction = knn.predict(testing_data)
+
+                context = {
+                    'crop_name':str.capitalize(str(final_prediction[0])),
+                    'accuracy': round(knn.score(X_test, y_test) * 100, 2)
+                }
+                return render(request, 'thanks-1.html', context)
+            except:
+                return HttpResponse(404)
+        else:
+            try:
+                received_data = [dict['state'], dict['district'], dict['crop'], dict['year'], dict['season']]
+                testing_data = [np.array(received_data, dtype='float64')]
+
+                df_yield = pd.read_csv('main_app/csvfiles/yield_processing_1.csv')
+
+                X = df_yield.drop(columns=['Unnamed: 0', 'Yield'], axis=1)
+                Y = df_yield['Yield']
+
+                X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .20, random_state=42)
+
+                regr = DecisionTreeRegressor(random_state = 42) # Do not use fit_intercept = False if you have removed 1 column after dummy encoding
+                regr.fit(X_train, Y_train)
+               
+                final_prediction = regr.predict(testing_data)
+
+                predictedacc = regr.predict(X_test)
+                r2_test = metrics.r2_score(Y_test, predictedacc)
+
+                context = {
+                    'yield': round(final_prediction[0], 3),
+                    'isYield':True,
+                    'netprod':round(float(dict['area']) * float(final_prediction[0])),
+                    'accuracy': round(r2_test * 100, 2)
+                }
+                return render(request, 'thanks-1.html', context)
+            except:
+                return HttpResponse(404)
+    else :
+        # Add Prediction Years
+        sam = pd.Index(['2020-21', '2021-22'])
+        yearsNew = years.append(sam)
+
+        # Send Data to Website
+        context = {
+            'states':states,
+            'districts':districts,
+            'crops':cropcats,
+            'years':yearsNew,
+            'seasons':seasons,
+        }
+        return render(request, 'index.html', context)
