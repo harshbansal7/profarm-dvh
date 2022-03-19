0a095213b5d8d3de1b922b82cf1bd5f6d2273059
Harsh Bansal <harshbansal.contact@gmail.com>
2022-03-19 17:38:33 +0530

    Settings and URLs updated

--- dfhmain/dfhmain/settings.py
+++ dfhmain/dfhmain/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,17 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3%b3o@4yf54_3kq=*j5kpcenee55lca&@hh!0sfj+aykhjxc%'
+SECRET_KEY = 'django-insecure-s&jgo^(y##(ajj^gh*d8f1jap=2_omkrx+-w#(q$lidno*vubb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app.apps.MainAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,8 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- dfhmain/dfhmain/urls.py
+++ dfhmain/dfhmain/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import path, include
+
 urlpatterns = [
+    path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
 ]
